527374b4122c2d615722d42dee85e681f472cd5d
saber <1428132225@qq.com>
2020-09-12 11:35:57 +0800

    add debug output

--- lib/req.go
+++ lib/req.go
@@ -57,9 +57,7 @@ func (r *req) SetHeader(header string) {
 //执行请求，并返回状态码、内容、错误信息
 func (r *req) Do() (int, string, error) {
 	s, _ := ioutil.ReadAll(r.request.Body) //把  body 内容读入字符串 s
-    log.Println(
-		"请求数据：", string(s)
-	)
+    log.Println("请求数据：", string(s))
 	resp, err := r.client.Do(r.request)
 	if err != nil {
 		return -1, "请求执行出错", err
